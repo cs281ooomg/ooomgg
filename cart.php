f4ac1a9e77218f02cce3b47885b35eccd44dde7a
ChiraksaPC <ChiraksaPC@ChiraksaPC>
2018-04-16 01:46:20 +0700

    upppp

--- cart.php
+++ cart.php
@@ -98,11 +98,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 								<td class="invert">
 									<div class="quantity">
 										<div class="quantity-select">
-											<div class="entry value-minus">&nbsp;</div>
 											<div class="entry value">
 												<span>1</span>
 											</div>
-											<div class="entry value-plus active">&nbsp;</div>
 										</div>
 									</div>
 								</td>
@@ -116,57 +114,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 								</td>
 							</tr>
-							<tr class="rem2">
-								<td class="invert">2</td>
-								<td class="invert-image"><a href="single.html"><img
-										src="images/s5.jpg" alt=" " class="img-responsive"></a></td>
-								<td class="invert">
-									<div class="quantity">
-										<div class="quantity-select">
-											<div class="entry value-minus">&nbsp;</div>
-											<div class="entry value">
-												<span>1</span>
-											</div>
-											<div class="entry value-plus active">&nbsp;</div>
-										</div>
-									</div>
-								</td>
-								<td class="invert">Red Bellies</td>
-
-								<td class="invert">$325.00</td>
-								<td class="invert">
-									<div class="rem">
-										<div class="close2"></div>
-									</div>
-
-								</td>
-							</tr>
-							<tr class="rem3">
-								<td class="invert">3</td>
-								<td class="invert-image"><a href="single.html"><img
-										src="images/s2.jpg" alt=" " class="img-responsive"></a></td>
-								<td class="invert">
-									<div class="quantity">
-										<div class="quantity-select">
-											<div class="entry value-minus">&nbsp;</div>
-											<div class="entry value">
-												<span>1</span>
-											</div>
-											<div class="entry value-plus active">&nbsp;</div>
-										</div>
-									</div>
-								</td>
-								<td class="invert">Chikku Loafers</td>
-
-								<td class="invert">$405.00</td>
-								<td class="invert">
-									<div class="rem">
-										<div class="close3"></div>
-									</div>
-
-								</td>
-							</tr>
-
 						</tbody>
 					</table>
 				</div>
@@ -175,8 +122,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 						<h4>Continue to basket</h4>
 						<ul>
 							<li>Product1 <i>-</i> <span>$675.00 </span></li>
-							<li>Product2 <i>-</i> <span>$325.00 </span></li>
-							<li>Product3 <i>-</i> <span>$405.00 </span></li>
 							<li>Total Service Charges <i>-</i> <span>$55.00</span></li>
 							<li>Total <i>-</i> <span>$1405.00</span></li>
 						</ul>
@@ -239,133 +184,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				</div>
 			</div>
 		</div>
-		<!-- //top products -->
-		<div class="mid_slider_w3lsagile">
-			<div class="col-md-3 mid_slider_text">
-				<h5>Some More Shoes</h5>
-			</div>
-			<div class="col-md-9 mid_slider_info">
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to="0" class=""></li>
-						<li data-target="#myCarousel" data-slide-to="1" class="active"></li>
-						<li data-target="#myCarousel" data-slide-to="2" class=""></li>
-						<li data-target="#myCarousel" data-slide-to="3" class=""></li>
-					</ol>
-					<div class="carousel-inner" role="listbox">
-						<div class="item">
-							<div class="row">
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g1.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g2.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g3.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g4.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="item active">
-							<div class="row">
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g5.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g6.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g2.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g1.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<div class="row">
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g1.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g2.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g3.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g4.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<div class="row">
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g1.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g2.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g3.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-								<div class="col-md-3 col-sm-3 col-xs-3 slidering">
-									<div class="thumbnail">
-										<img src="images/g4.jpg" alt="Image" style="max-width: 100%;">
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<a class="left carousel-control" href="#myCarousel" role="button"
-						data-slide="prev"> <span class="fa fa-chevron-left"
-						aria-hidden="true"></span> <span class="sr-only">Previous</span>
-					</a> <a class="right carousel-control" href="#myCarousel"
-						role="button" data-slide="next"> <span class="fa fa-chevron-right"
-						aria-hidden="true"></span> <span class="sr-only">Next</span>
-					</a>
-					<!-- The Modal -->
-
-				</div>
-			</div>
-
-			<div class="clearfix"></div>
-		</div>
 	</div>
+	
+	<!-- top prodcut -->
+	<?php require 'includes/top_product.php';?>
+	<!-- //top product -->
+	
 	<!-- footer -->
 		<?php require 'includes/footer.php';?>
 	   <!-- //footer -->
@@ -398,48 +222,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	<!--search-bar-->
 	<script src="js/search.js"></script>
 	<!--//search-bar-->
-	<!--quantity-->
-	<script>
-		$('.value-plus').on('click', function () {
-			var divUpd = $(this).parent().find('.value'),
-				newVal = parseInt(divUpd.text(), 10) + 1;
-			divUpd.text(newVal);
-		});
-
-		$('.value-minus').on('click', function () {
-			var divUpd = $(this).parent().find('.value'),
-				newVal = parseInt(divUpd.text(), 10) - 1;
-			if (newVal >= 1) divUpd.text(newVal);
-		});
-	</script>
-	<!--quantity-->
-	<script>
-		$(document).ready(function (c) {
-			$('.close1').on('click', function (c) {
-				$('.rem1').fadeOut('slow', function (c) {
-					$('.rem1').remove();
-				});
-			});
-		});
-	</script>
-	<script>
-		$(document).ready(function (c) {
-			$('.close2').on('click', function (c) {
-				$('.rem2').fadeOut('slow', function (c) {
-					$('.rem2').remove();
-				});
-			});
-		});
-	</script>
-	<script>
-		$(document).ready(function (c) {
-			$('.close3').on('click', function (c) {
-				$('.rem3').fadeOut('slow', function (c) {
-					$('.rem3').remove();
-				});
-			});
-		});
-	</script>
 
 	<!-- start-smoth-scrolling -->
 	<script type="text/javascript" src="js/move-top.js"></script>
