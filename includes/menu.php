cf824530958021305e9daa836d4c80ca96fc4670
pattarabank <pattarabank@DESKTOP-S07V2LF>
2018-04-17 06:26:48 +0700

    fix addProduct button and addCatagory button

--- includes/menu.php
+++ includes/menu.php
@@ -13,10 +13,12 @@
 		<?php } ?>
 		<li><a href="about.php">About</a></li>
 		<li><a href="404.php">Team</a></li>
-		<?php// if ($acc->getTYPE() == '1'){ ?>
+		<?php if ($session_set){?>
+		<?php if ($acc->getTYPE() == '1'){ ?>
 		<li><a href="add_Catagory.php">Add Catagory</a></li>
 		<li><a href="add_Product.php">Add Product</a></li>
-		<?php// } ?>
+		<?php } ?>
+		<?php }?>
 		<li><a href="product.php">Shop Now</a></li>
 		<li><a href="contact.php">Contact</a></li>
 		<?php if($session_set){ ?>
