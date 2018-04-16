9fd7cc377d768309d4f77852ff6697907fee942f
fookza2013 <fookza2013@DESKTOP-S07V2LF>
2018-04-17 06:07:24 +0700

    fix

--- includes/menu.php
+++ includes/menu.php
@@ -15,7 +15,7 @@
 		<li><a href="404.php">Team</a></li>
 		<?php if ($acc->getTYPE() == '1'){ ?>
 		<li><a href="add_Catagory.php">Add Catagory</a></li>
-		<li><a href="add_Product.php"></a>Add Product</li>
+		<li><a href="add_Product.php">Add Product</a></li>
 		<?php } ?>
 		<li><a href="product.php">Shop Now</a></li>
 		<li><a href="contact.php">Contact</a></li>
