233697cb54af71d0e26523d4e0796f18641c22b6
pakon <pakon@DomePakornNB>
2018-04-18 10:49:12 +0700

    edit bug nidnung

--- includes/menu.php
+++ includes/menu.php
@@ -15,8 +15,8 @@
 		<li><a href="404.php">Team</a></li>
 		<?php if ($session_set){?>
 		<?php if ($acc->getTYPE() == '1'){ ?>
-		<li><a href="add_Catagory.php">Add Catagory</a></li>
-		<li><a href="add_Product.php">Add Product</a></li>
+		<li><a href="add_catagory.php">Add Catagory</a></li>
+		<li><a href="add_product.php">Add Product</a></li>
 		<?php } ?>
 		<?php }?>
 		<li><a href="product.php">Shop Now</a></li>
