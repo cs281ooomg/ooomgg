2f2202ddd17b2d8d4702c5db0f5710b813bac89f
pakon <pakon@DomePakornNB>
2018-05-13 19:15:53 +0700

    update Show Order Page

---
+++ order.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+	  <meta charset="UTF-8">
+	  <title>OOOMG Order #001</title>
+	  <link rel="stylesheet" href="css/order.css">
+	</head>
+<body> 
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="order.css">
+		<link rel="license" href="https://www.opensource.org/licenses/mit-license/">
+	</head>
+	<body>
+		<header>
+			<h1>OOOMG Order #001</h1>
+			<address>
+				<p>Jonathan Neal</p>
+				<p>101 E. Chapman Ave<br>Orange, CA 92866</p>
+				<p>(800) 555-1234</p>
+			</address>
+			<span><img alt="" src="http://www.jonathantneal.com/examples/invoice/logo.png"></span>
+		</header>
+		<article>
+			<h1>Recipient</h1>
+			<address>
+				<p>Some Company<br>c/o Some Guy</p>
+			</address>
+			<table class="meta">
+				<tr>
+					<th><span>Invoice #</span></th>
+					<td><span>101138</span></td>
+				</tr>
+				<tr>
+					<th><span>Date</span></th>
+					<td><span>January 1, 2012</span></td>
+				</tr>
+				<tr>
+					<th><span>Amount Due</span></th>
+					<td><span id="prefix">$</span><span>600.00</span></td>
+				</tr>
+			</table>
+			<table class="inventory">
+				<thead>
+					<tr>
+						<th><span>Item</span></th>
+						<th><span>Description</span></th>
+						<th><span>Rate</span></th>
+						<th><span>Quantity</span></th>
+						<th><span>Price</span></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><span>Front End Consultation</span></td>
+						<td><span>Experience Review</span></td>
+						<td><span data-prefix>$</span><span>150.00</span></td>
+						<td><span>4</span></td>
+						<td><span data-prefix>$</span><span>600.00</span></td>
+					</tr>
+					<tr>
+						<td><span>Front End Consultation</span></td>
+						<td><span>Experience Review</span></td>
+						<td><span data-prefix>$</span><span>150.00</span></td>
+						<td><span>4</span></td>
+						<td><span data-prefix>$</span><span>600.00</span></td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="balance">
+				<tr>
+					<th><span>Total</span></th>
+					<td><span data-prefix>$</span><span>600.00</span></td>
+				</tr>
+				<tr>
+					<th><span>Vat 7%</span></th>
+					<td><span data-prefix>$</span><span>0.00</span></td>
+				</tr>
+				<tr>
+					<th><span>Balance Due</span></th>
+					<td><span data-prefix>$</span><span>600.00</span></td>
+				</tr>
+			</table>
+		</article>
+		<aside>
+			<h1><span>Additional Notes</span></h1>
+			<div>
+				<p>A finance charge of 1.5% will be made on unpaid balances after 30 days.</p>
+			</div>
+		</aside>
+	</body>
+</html>
+  
+</body>
+
+</html>
