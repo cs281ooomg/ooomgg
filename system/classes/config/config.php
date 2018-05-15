aa139ce12b979fa059b5e7d744006e7e71574f5b
pakon <pakon@DomePakornNB>
2018-05-15 16:15:14 +0700

    upp

--- system/classes/config/config.php
+++ system/classes/config/config.php
@@ -1,6 +1,6 @@
 <?php
-    $hostname = '192.168.1.10:3307'; //inside
-    //$hostname = 'server.mydpk.net:3307'; //outside
+    //$hostname = '192.168.1.10:3307'; //inside
+    $hostname = 'server.mydpk.net:3307'; //outside
     $username = 'ooomg';
     $password = '12345678';
     $dbname = 'ooomg';
