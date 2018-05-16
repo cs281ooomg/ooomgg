4fc5865dd9da3572a76d44813cd076c9823c2ac4
PC PARE <PC PARE@DESKTOP-IITKQPV>
2018-05-17 01:52:23 +0700

    eiei

--- order.php
+++ order.php
@@ -1,8 +1,21 @@
+
+<?php
+require 'includes/autoload.php';
+if (!$session_set) {
+	echo "<script language=\"JavaScript\">";
+	echo "alert('Please login!!!')";
+	echo "</script>";
+	echo "<script> document.location.href=\"login.php\";</script>";
+	exit();
+} 
+$ord = OrderMgnt::getOrd($acc);
+$cart = CartMgnt::getCartByAccount($account);
+?>
 <!DOCTYPE html>
 <html lang="en" >
 	<head>
 	  <meta charset="UTF-8">
-	  <title>OOOMG Order #001</title>
+	  <title>OOOMG Order #<?php echo $ord->getIndex();?></title>
 	  <link rel="stylesheet" href="css/order.css">
 	</head>
 <body> 
@@ -14,72 +27,79 @@
 	</head>
 	<body>
 		<header>
-			<h1>OOOMG Order #001</h1>
+			<h1>OOOMG Order #<?php echo $ord->getIndex();?></h1>
 			<address>
-				<p>Jonathan Neal</p>
-				<p>101 E. Chapman Ave<br>Orange, CA 92866</p>
-				<p>(800) 555-1234</p>
+				<?php 	$addac = OrderMgnt::getACC($acc); ?>
+				<p><?php echo $addac->getFNAME();?>  <?php echo $addac->getLNAME();?></p>
+				<?php 	$addor = OrderMgnt::getAddress($acc); ?>
+				<p><?php echo $addor->getInfo();?><br><?php echo $addor->getDistrict(); ?> <?php echo $addor->getSubDis();?>  <?php echo $addor->getProvince(); ?><br><?php echo $addor->getAddCode();?></p>
+				<p><?php echo $addac->getTEL();?></p>
 			</address>
 			<span><img alt="" src="http://www.jonathantneal.com/examples/invoice/logo.png"></span>
 		</header>
 		<article>
 			<h1>Recipient</h1>
 			<address>
-				<p>Some Company<br>c/o Some Guy</p>
+				<p><?php echo $addac->getFNAME();?><br><?php echo $addac->getLNAME();?></p>
 			</address>
 			<table class="meta">
 				<tr>
 					<th><span>Invoice #</span></th>
-					<td><span>101138</span></td>
+					<td><span><?php echo $ord->getIndex();?></span></td>
 				</tr>
 				<tr>
 					<th><span>Date</span></th>
-					<td><span>January 1, 2012</span></td>
+					<td><span><?php echo $todays_date = date("d-m-Y");?></span></td>
 				</tr>
-				<tr>
-					<th><span>Amount Due</span></th>
-					<td><span id="prefix">$</span><span>600.00</span></td>
-				</tr>
+				
 			</table>
 			<table class="inventory">
 				<thead>
 					<tr>
 						<th><span>Item</span></th>
-						<th><span>Description</span></th>
+						
 						<th><span>Rate</span></th>
 						<th><span>Quantity</span></th>
 						<th><span>Price</span></th>
 					</tr>
 				</thead>
 				<tbody>
+			<?php
+                                $total = 0;
+                                $cartMgnt = new CartMgnt();
+                                if ($cart != NULL) {
+                                    $i = 1;
+                                    $items = $cart->getItems();
+                                    foreach ($items as $cartItem){
+                                        $product = $cartItem->getProduct();
+                                        $quantity = $cartItem->getQuantity();
+                                    ?>
 					<tr>
-						<td><span>Front End Consultation</span></td>
-						<td><span>Experience Review</span></td>
-						<td><span data-prefix>$</span><span>150.00</span></td>
-						<td><span>4</span></td>
-						<td><span data-prefix>$</span><span>600.00</span></td>
+						<td><span><?php echo $product->getPName();?></span></td>
+						
+						<td><span data-prefix>$</span><span><?php echo $product->getPPrice();?></span></td>
+						<td><span><?php echo $quantity;?></span></td>
+						<td><span data-prefix>$</span><span><?php echo $product->getPPrice()*$quantity;?></span></td>
 					</tr>
-					<tr>
-						<td><span>Front End Consultation</span></td>
-						<td><span>Experience Review</span></td>
-						<td><span data-prefix>$</span><span>150.00</span></td>
-						<td><span>4</span></td>
-						<td><span data-prefix>$</span><span>600.00</span></td>
-					</tr>
+                          <?php		$total = $total+($product->getPPrice()*$quantity);
+                                    $i ++;
+                                    }
+                                }
+                                ?>   
 				</tbody>
 			</table>
 			<table class="balance">
 				<tr>
 					<th><span>Total</span></th>
-					<td><span data-prefix>$</span><span>600.00</span></td>
+					<td><span data-prefix>$</span><span><?php echo $total;?></span></td>
 				</tr>
 				<tr>
 					<th><span>Vat 7%</span></th>
-					<td><span data-prefix>$</span><span>0.00</span></td>
+					<td><span data-prefix>$</span><span><?php echo $total*(7/100);?></span></td>
 				</tr>
 				<tr>
 					<th><span>Balance Due</span></th>
-					<td><span data-prefix>$</span><span>600.00</span></td>
+					<td><span data-prefix>$</span><span><?php echo $total+$total*(7/100);?></span></td>
 				</tr>
 			</table>
 		</article>
--- system/classes/OrderMgnt.php
+++ system/classes/OrderMgnt.php
@@ -81,5 +81,72 @@ class OrderMgnt
         }
         return null;
     }
+    public static function getAddress($acc)
+    {
+    	require 'config/config.php';
+    	$conn = new mysqli($hostname, $username, $password, $dbname);
+    	$sql = "SELECT * FROM ADDRESS WHERE ACC_ID ='" . $acc->getID() . "'";
+    	$query = $conn->query($sql);
+    	$result = $query->fetch_assoc();
+    	if($result){
+    		$address = new Address($result["ADD_INDEX"], $result["ACC_ID"], $result["ADD_INFO"], $result["ADD_SUB_DISTRICT"], $result["ADD_DISTRICT"], $result["ADD_PROVINCE"], $result["ADD_CODE"], $result["DATE"]);
+    		return $address;
+    	}else{
+    		return NULL;
+    	}
+    }
+    public static function getACC($acc)
+    {
+    	require 'config/config.php';
+    	$conn = new mysqli($hostname, $username, $password, $dbname);
+    	$sql = "SELECT * FROM ACCOUNT WHERE ACC_ID ='" . $acc->getID() . "'";
+    	$query = $conn->query($sql);
+    	$result = $query->fetch_assoc();
+    	if($result){
+    		$acc = new Account($result["ACC_ID"],$result["ACC_PASS"], $result["ACC_TYPE"], $result["ACC_EMAIL"], $result["ACC_FNAME"], $result["ACC_LNAME"], $result["ACC_GENDER"], $result["ACC_TEL"], $result["ACC_POINT"]);
+    		return $acc;
+    	}else{
+    		return NULL;
+    	}
+    }
+    public static function getOrd($acc)
+    {
+    	require 'config/config.php';
+    	$conn = new mysqli($hostname, $username, $password, $dbname);
+    	$sql = "SELECT * FROM ORDERS WHERE ACC_ID ='" . $acc->getID() . "'";
+    	$query = $conn->query($sql);
+    	$result = $query->fetch_assoc();
+    	if($result){
+    		$ord = new Order($result["ORDER_INDEX"], $result["ORDER_DATE"],$result["ACC_ID"], $result["ORDER_CODE"], $result["ORDER_STATUS"]);
+    		return $ord;
+    	}else{
+    		return NULL;
+    	}
+    }
+    
+    public static function getProduct($acc){
+    	require 'config/config.php';
+    	$conn = new mysqli($hostname, $username, $password, $dbname);
+    	$sql = "SELECT * FROM ORDERS  WHERE ACC_ID ='" . $acc->getID() . "'";
+    	$query = $conn->query($sql);
+    	$result = $query->fetch_assoc();
+    	$sql2 = "SELECT * FROM ORDER_ITEMS WHERE ORDER_INDEX = '" . $result['ORDER_INDEX']. "' ";
+    	$query2 = $conn->query($sql2);
+    	$resultArray = array();
+    	$count = 0;
+    	while ($result2 = $query->fetch_array()) {
+    		
+    		$product = ProductMgnt::getProduct($result2['PRO_INDEX']);
+    		$quantity = $result2['QUANTITY'];
+    		$orderItem = new OrderItem($product, $quantity);
+    		$resultArray[] = $orderItem;
+    		$count ++;
+    	}
+    	if ($count === 0) {
+    		return NULL;
+    	}
+    	//$ordd = new Order($result['ORDER_INDEX'], $resultArray);
+    	return $resultArray;
+    }
 }
 ?>
